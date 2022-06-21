7d142c9d0f9de67d9324b7f4e9086cb71d6e04e6
Joyce Shendre <joyce.shendre@konverge.ai>
2022-06-21 11:23:55 +0530

    smtp credentials

--- user_login_sys/info.py
+++ user_login_sys/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'joycels062016@gmail.com'
-EMAIL_HOST_PASSWORD = 'aaoggjmmfkgtueam'
+EMAIL_HOST_USER = 'abc@gmail.com'
+EMAIL_HOST_PASSWORD = 'your host password'
 EMAIL_PORT = 587
